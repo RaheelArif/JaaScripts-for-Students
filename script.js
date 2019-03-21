43500d7d8b4dd7b34dda0418cbc8ca08db4cccb5
RaheelArif <phuawei292@gmail.com>
2019-03-21 14:57:29 +0500

    if statement

--- script.js
+++ script.js
@@ -1,11 +1,30 @@
-var firstName = prompt( "enter your first name", "imran khan");
-alert("your name is " + firstName)
+
+
+var x = 50;
+var y = prompt("enter a number")
+var scoor = 0;
+if ( x > y){
+    alert("your value is less then 50") ;
+    scoor++;
+
+}
+alert("your scoor is = " + scoor)
 
 
 
 
 
-// var firstName = "abc" ;
-// var lastName = "xyz" ;
-// var sum = 5 + 3;
-// alert ("5 + 3 is equal to  " + sum ) ;
+// var country = "Pakistan" ;
+// var myCountry = prompt("enter your city");
+// if ( country !== myCountry) {
+//     alert("congratulations you are in the country " + myCountry);
+// }
+
+
+
+
+// var country = "Pakistan" ;
+// var myCountry = prompt("enter your city");
+// if ( country === myCountry) {
+//     alert("congratulations you are in Islamic country , that is " + myCountry);
+// }

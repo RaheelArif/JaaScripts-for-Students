64e597697ac9b4a12844b9e79bc3c0da1a2bd407
RaheelArif <phuawei292@gmail.com>
2019-03-26 14:44:15 +0500

    1test questions

--- script.js
+++ script.js
@@ -1,54 +1,15 @@
-var SAT = 5;
-var avg = 4;
-var GPA = 5;
-var sport = 5;
+var rollno = prompt("enter your roll no");
+if(100 > rollno){
+    alert("old student")
+}else{
+    alert("new student")
+} ;
 
 
 
 
-if (SAT > avg || GPA > "aksd fkjahsd fk" || sport === "football") {
-alert("Welcome to Bubba State!");
-}
-else {
-alert("Have you looked into appliance repair?");
-}
 
-
-
-// var rehman = prompt("rehman is there?  yess or no ");
-
-// var suit = prompt("his suit colour is white ? yess or no");
-
-// if(rehman === "yess" ||  suit === "yess" ) {
-//     alert("throw ink on him");
-// }else{
-//     alert("give him 2000 RS so he can buy a suit")
-// }
-
-
-
-
-// var rehman = prompt("rehman is there?  yess or no ");
-
-// var suit = prompt("his suit colour is white ? yess or no");
-
-// if(rehman === "yess" && suit === "yess" ) {
-//     alert("throw ink on him");
-// }else{
-//     alert("give him 2000 RS so he can buy a suit")
-// }
-
-
-
-// var a = 5;
-// var b = prompt("enter a number");
-// var scoor = 0;
-// if(a > b){
-//     alert("your number is les then 5")
-//     scoor++;
-
-// } else{
-//     alert("your number is greater then 5")
-//     scoor--;
-// }
-// alert("your scoor is = " + scoor);
+// var username =  prompt("enter you username");
+// var fathername =  prompt("enter your father name");
+// var rollno = prompt("enter your roll no");
+// alert("your name is " + username + " your father name is" + fathername + "your roll number is " + rollno) ;

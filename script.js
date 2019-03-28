dab18f41a2c903d1d046bd65f3972c5aba5cff34
RaheelArif <phuawei292@gmail.com>
2019-03-28 15:00:07 +0500

    arrays

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-var rollno = prompt("enter your roll no");
-if(100 > rollno){
-    alert("old student")
-}else{
-    alert("new student")
-} ;
+var city0 = "Atlanta";
+var city1 = "Baltimore";
+var city2 = "Chicago";
+var city3 = "Denver";
+var city4 = "Los Angeles";
+var city5 = "Seattle";
+var cities = [ "jaranwala", "faisalabad", "a", "b", "c"]
+alert(cities[4])
 
 
 
+// var student = prompt("student");
+// var gender = prompt("gender");
 
 
-// var username =  prompt("enter you username");
-// var fathername =  prompt("enter your father name");
-// var rollno = prompt("enter your roll no");
-// alert("your name is " + username + " your father name is" + fathername + "your roll number is " + rollno) ;
+// if (gender === "male" || gender === "female" && student === "web development"){
+//      alert("beth jay")
+// } else{
+//     alert("nai aa sakta")
+// }
+
+// if (student === "web development"){
+//     if(gender === "male"){
+//         alert("beth jay")
+//     }else if(gender === "female"){
+//         alert("beth jay")
+//     }else{
+//         alert("nai aa sakta")
+//     }
+
+// }else{
+//     alert("nai aa sakta bilkl b ")
+// }
+

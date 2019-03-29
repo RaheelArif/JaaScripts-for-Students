4b4bf93c0488b5a1b636e65fbbe3e0495083fff0
RaheelArif <phuawei292@gmail.com>
2019-03-29 21:18:01 +0500

    arrays push pop

--- script.js
+++ script.js
@@ -1,34 +1,7 @@
-var city0 = "Atlanta";
-var city1 = "Baltimore";
-var city2 = "Chicago";
-var city3 = "Denver";
-var city4 = "Los Angeles";
-var city5 = "Seattle";
-var cities = [ "jaranwala", "faisalabad", "a", "b", "c"]
-alert(cities[4])
-
-
-
-// var student = prompt("student");
-// var gender = prompt("gender");
-
-
-// if (gender === "male" || gender === "female" && student === "web development"){
-//      alert("beth jay")
-// } else{
-//     alert("nai aa sakta")
-// }
-
-// if (student === "web development"){
-//     if(gender === "male"){
-//         alert("beth jay")
-//     }else if(gender === "female"){
-//         alert("beth jay")
-//     }else{
-//         alert("nai aa sakta")
-//     }
-
-// }else{
-//     alert("nai aa sakta bilkl b ")
-// }
+var countries = [];
+countries[0] = "india";
+countries[1] = "china"
+countries[2] = "pakistan";
+countries.pop();
+alert(countries[2])
 

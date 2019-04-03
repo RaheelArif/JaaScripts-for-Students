c3a348dbda2b2e1c28875e688ef2fac9ba91a235
RaheelArif <phuawei292@gmail.com>
2019-04-03 14:36:47 +0500

    nested loop , uppercase and lowercase

--- script.js
+++ script.js
@@ -1,40 +1,16 @@
-var cities = ["jrw", "fsd", "isb", "karachi" , "multan", "lahore"];
-
-console.log("console window")
-
-var name = false;
-for( var i = 0; i < cities.length; i++ ){
-    if(cities[i] === "isb"){
-        name = true; 
-    }
-
-    if(name === true){
-       alert("value found ") 
-    }else{
-        alert("value not found")
-    }
-
-}
+var name = prompt("enter your name");
+var updatedName = name.toUpperCase();
+alert(updatedName);
 
 
 
 
+// for(var i = 0; i <5; i++ ){
 
-
-
-
-// for( var i = 0; i < cities.length; i++ ){
-//    console.log(i)
+//     for(var j = 0; j <3 ; j++){
+//         console.log(j + " " + i)
+//     }
 // }
-
-
-// alert(cities[0]);
-// alert(cities[1]);
-// alert(cities[2]);
-// alert(cities[3]);
-
-
-
 
 
 

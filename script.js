653d7433ca050c2a664518c25460eb30192d33f3
RaheelArif <phuawei292@gmail.com>
2019-04-04 15:04:02 +0500

    firn character indexof etc

--- script.js
+++ script.js
@@ -1,16 +1,35 @@
-var name = prompt("enter your name");
-var updatedName = name.toUpperCase();
-alert(updatedName);
+var a = "we are learning javascript!";
+var found = prompt("enter a letter to find in this string")
+for(var i = 0; i <= a.length; i++ ){
+    if(a.charAt(i) === found){
+        alert("value found")
+        break;
+    }
+}
 
 
 
 
-// for(var i = 0; i <5; i++ ){
+// console.log(a.charAt(4));
 
-//     for(var j = 0; j <3 ; j++){
-//         console.log(j + " " + i)
-//     }
-// }
+
+// console.log(a.lastIndexOf(" "))
+
+
+// console.log(a.length);
+// alert(a.indexOf("javascript"))
+
+
+
+
+
+// console.log(a.slice(3,6));
+
+// console.log(a.slice(3));
+
+
+
+
 
 
 

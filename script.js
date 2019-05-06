35d3b172d3222a5d97d1c3143e30e0c434f4f141
RaheelArif <phuawei292@gmail.com>
2019-05-06 10:26:39 +0500

    49 to 55 chapter

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
 
+
+
+
+
+
+
+
+
+
+
+
+function changeColor(){
+    var number = Math.random();
+    var updatedNumber = Math.floor((number * 6) + 1);
+    var a = document.getElementById("h1");
+    switch(updatedNumber){
+        case 1:
+        a.className = "one";
+        break;
+        case 2:
+        a.className = "two";
+        break;
+        case 3:
+        a.className = "three";
+        break;
+        case 4:
+        a.className = "four";
+        break;
+        case 5:
+        a.className = "five";
+        break;
+        default:
+        a.className = "six";
+
+
+    }
+}
+
+
+
+
+
+
+function abc(b){
+
+    var a =  document.getElementById(b).value ;
+   document.getElementById("settingValue").value = a;
+}
